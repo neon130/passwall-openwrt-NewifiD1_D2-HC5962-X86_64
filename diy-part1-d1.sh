3c85449f3004aa672d5eba8572ab389ab776f7fa
ChenChen <cc2286927@sina.cn>
2021-02-12 23:09:17 +0800

    Add files via upload

---
+++ diy-part1-d1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/3.1.1.1/g' package/base-files/files/bin/config_generate
+# mkdir package/base-files/files/config
+# echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+# rm -rf package/lean/smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
