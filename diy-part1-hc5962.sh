be3dab5a6839d2b0208f89e5e83aa893c2534c18
ChenChen <cc2286927@sina.cn>
2021-02-24 10:08:39 +0800

    Update diy-part1-hc5962.sh

--- diy-part1-hc5962.sh
+++ diy-part1-hc5962.sh
@@ -13,6 +13,4 @@ sed -i 's/192.168.1.1/4.1.1.1/g' package/base-files/files/bin/config_generate
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 # git clone https://github.com/kenzok8/small.git package/small
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
