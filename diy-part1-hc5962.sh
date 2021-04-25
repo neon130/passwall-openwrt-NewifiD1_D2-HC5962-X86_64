52e51f68076c5a1b0f069c9d216c2ecf82c4918d
ChenChen <cc2286927@sina.cn>
2021-04-26 01:15:59 +0800

    Update diy-part1-hc5962.sh

--- diy-part1-hc5962.sh
+++ diy-part1-hc5962.sh
@@ -11,9 +11,9 @@ sed -i 's/192.168.1.1/4.1.1.1/g' package/base-files/files/bin/config_generate
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 # rm -rf package/lean/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 git clone https://github.com/garypang13/smartdns-le package/smartdns-le
 wget -P package/lua-maxminddb https://raw.githubusercontent.com/garypang13/openwrt-packages/master/lua-maxminddb/Makefile
