d13a22151a234e22b241793ca5e3b9e27bbb823e
ChenChen <cc2286927@sina.cn>
2021-04-26 16:21:56 +0800

    Update diy-part1-d1.sh

--- diy-part1-d1.sh
+++ diy-part1-d1.sh
@@ -13,7 +13,8 @@ sed -i 's/192.168.1.1/3.1.1.1/g' package/base-files/files/bin/config_generate
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 # git clone https://github.com/kenzok8/small.git package/small
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 git clone https://github.com/garypang13/smartdns-le package/smartdns-le
 wget -P package/lua-maxminddb https://raw.githubusercontent.com/garypang13/openwrt-packages/master/lua-maxminddb/Makefile
