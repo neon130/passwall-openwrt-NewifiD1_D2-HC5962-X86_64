43b5d6e81328d7708f7d5590f16a3ce17df0592c
ChenChen <cc2286927@sina.cn>
2021-04-29 08:27:15 +0800

    Update diy-part1-d2.sh

--- diy-part1-d2.sh
+++ diy-part1-d2.sh
@@ -7,6 +7,8 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/6.1.1.1/g' package/base-files/files/bin/config_generate
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 # mkdir package/base-files/files/config
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 # rm -rf package/lean/smartdns
